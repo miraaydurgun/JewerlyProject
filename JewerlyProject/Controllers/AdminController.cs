9e8a304e4fdf9368339bb5af0a6c16b55f027cb0
miraaydurgun <miraydurgun67@gmail.com>
2023-01-09 14:59:36 +0300

    layout oluşturuldu ve frontentte düzeltmeler yapuıldı

---
+++ JewerlyProject/Controllers/AdminController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JewelryProject.Controllers
+{
+    public class AdminController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public PartialViewResult PartialSideBar()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialFooter()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialHead()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialScript()
+        {
+            return PartialView();
+        }
+    }
+}

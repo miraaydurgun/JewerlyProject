823ee6afc8893f4ecd0e443eeb048843191ce5cc
miraaydurgun <miraydurgun67@gmail.com>
2023-01-11 22:13:31 +0300

    Dal katmanında; GenericDal, GenericRepository, Abstract yapıldı. Business Katmanında; Abstract yapıldı Concrete manager eklemeye devam edilmesi gerek sadece 3 adet eklendi.

---
+++ BusinessLayer/Concrete/CampaignManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+
+namespace BusinessLayer.Concrete
+{
+    public class CampaignManager : ICampaignService
+    {
+        //
+        ICampaignDal _campaignDal; //ekleme silme güncelleme işlemleri için altı kısımda kullanılacak generic değişkeni tanımlandı
+        public CampaignManager(ICampaignDal campaignDal)
+        {
+            _campaignDal = campaignDal;
+        }
+
+        public void TAdd(Campaign t)
+        {
+            _campaignDal.Insert(t);
+        }
+
+        public void TDelete(Campaign t)
+        {
+            _campaignDal.Delete(t);
+        }
+
+        public Campaign TGetByID(int id)
+        {
+            return _campaignDal.GetByID(id);
+        }
+
+        public List<Campaign> TGetList()
+        {
+            return _campaignDal.GetAll();
+        }
+
+        public void TUpdate(Campaign t)
+        {
+            _campaignDal.Update(t);
+        }
+    }
+}
---
+++ BusinessLayer/Concrete/ContactManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+
+namespace BusinessLayer.Concrete
+{
+    public class ContactManager : IContactService
+    {
+        IContactDal _contactDal; //ekleme silme güncelleme işlemleri için altı kısımda kullanılacak generic değişkeni tanımlandı
+        public ContactManager(IContactDal contactDal)
+        {
+            _contactDal = contactDal;
+        }
+        public void TAdd(Contact t)
+        {
+            _contactDal.Insert(t);
+        }
+
+        public void TDelete(Contact t)
+        {
+            _contactDal.Delete(t);
+        }
+
+        public Contact TGetByID(int id)
+        {
+            return _contactDal.GetByID(id);
+        }
+
+        public List<Contact> TGetList()
+        {
+            return _contactDal.GetAll();
+        }
+
+        public void TUpdate(Contact t)
+        {
+            _contactDal.Update(t);
+        }
+    }
+}

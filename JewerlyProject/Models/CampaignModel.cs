db64b524dabd96f6a39fb383850bd1af8d57f6bf
miraaydurgun <miraydurgun67@gmail.com>
2023-01-16 12:15:43 +0300

    slider ve header arasındaki boşluk halloldu. Companents oluşturuldu kampanya ksımı yapıldı yalnızca image gelmiyor. newArrival ve Review için de component oluşturulması gerek ve slider dönmesi için hazır kodu diğer projeden alacağız.

---
+++ JewerlyProject/Models/CampaignModel.cs
@@ -0,0 +1,16 @@
+﻿using EntityLayer.Concrete;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace JewelryProject.Models
+{
+    public class CampaignModel
+    {
+        [Key]
+        public int Campaignid { get; set; }
+        public string CampaignImage { get; set; }
+        public string CampaignComment { get; set; }
+        public string CampaignTitle { get; set; }
+        public List<Home> Homes { get; set; }
+    }
+}
---
+++ JewerlyProject/ViewComponents/Home/CampaignListByHome.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JewelryProject.ViewComponents.Home
+{
+    public class CampaignListByHome:ViewComponent
+    {
+        CampaignManager hm = new CampaignManager(new EfCampaignRepository());
+        public IViewComponentResult Invoke()
+        {
+            var values = hm.TGetList();
+            return View(values);
+        }
+    }
+}

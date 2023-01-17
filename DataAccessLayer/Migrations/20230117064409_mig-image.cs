cd04696520578f2fa5dc4666af274a32523d3f5c
miraaydurgun <miraydurgun67@gmail.com>
2023-01-17 10:55:01 +0300

    componentler sayfaya ayarlandı (slider hariç). düzenlemeler yapıldı dataya bağlı geliyor. Reviev'de döndürme butonu yok sadece :)

---
+++ DataAccessLayer/Migrations/20230117064409_mig-image.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataAccessLayer.Migrations
+{
+    public partial class migimage : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ReviewImage",
+                table: "Reviews",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ReviewImage",
+                table: "Reviews");
+        }
+    }
+}

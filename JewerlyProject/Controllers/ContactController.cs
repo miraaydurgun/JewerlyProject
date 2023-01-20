a143cf948564430a24ca596374a72cbc9e66fe22
ZeynepBostan <zynpbos@gmail.com>
2023-01-20 15:00:08 +0300

    ContactController ve viewsde contact index oluşturuldu

---
+++ JewerlyProject/Controllers/ContactController.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JewelryProject.Controllers
+{
+    public class ContactController : Controller
+    {
+
+        ContactManager cm = new ContactManager(new EfContactRepository());
+        public IActionResult Index()
+        //Jewelry'lerin listelendiği sayfa olacak
+        {
+            var values = cm.TGetList();
+            return View(values);
+
+        }
+    
+    }
+}

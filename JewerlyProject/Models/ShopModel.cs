2742c904717ade3d85b63baba3e80e0fcf07307a
miraaydurgun <miraydurgun67@gmail.com>
2023-01-24 13:05:48 +0300

    shop_single component eklendi düzeltmeler yapıldı

---
+++ JewerlyProject/Models/ShopModel.cs
@@ -0,0 +1,23 @@
+﻿using EntityLayer.Concrete;
+using System.ComponentModel.DataAnnotations;
+
+namespace JewelryProject.Models
+{
+    public class ShopModel
+    {
+        [Key]
+        public int Shopid { get; set; }
+        public string ShopImage { get; set; }
+        public string ShopHover { get; set; }
+        public string ShopTitle { get; set; }
+        public decimal ShopPrice { get; set; }
+        public string ShopCartIcon { get; set; }
+        public bool ShopStatus { get; set; }
+
+
+        public int Shop_Singleid { get; set; }
+        public Shop_Single Shop_Single { get; set; }
+
+
+    }
+}
---
+++ JewerlyProject/ViewComponents/Shop_Single/ShopListByShop_Single.cs
@@ -0,0 +1,19 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using JewelryProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.Collections.Generic;
+
+namespace JewelryProject.ViewComponents.Shop_Single
+{
+    public class ShopListByShop_Single : ViewComponent
+    {
+        ShopManager sm = new ShopManager(new EfShopRepository());
+        public IViewComponentResult Invoke()
+        {
+            var values = sm.TGetList();
+            return View(values);
+        }
+    }
+}
